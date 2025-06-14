d2cd687ac30745e19916c42daf4dbcd8c7e262c4
code-shams <code.meshams@gmail.com>
2025-06-14 20:59:31 +0600

    1. Added JWT authentication using     a. Firebase access token     b. verifyToken middleware 2. Added a joined event api     Created an secure jwt get api for fetching user     specific events that the user has joined in.

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const admin = require("firebase-admin");
+const serviceAccount = require("./firebaseAdminKey.json");
 
 const port = process.env.PORT || 3000;
 const app = express();
 
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+});
+
+// ?Middlewares
 app.use(cors());
 app.use(express.json());
+
+const verifyToken = (req, res, next) => {
+    const token = req.headers?.authorization;
+    if (!token || !token.startsWith("Bearer ")) {
+        return res.status(401).send({ message: "Unauthorized Access Denied" });
+    }
+    const accessToken = token.split(" ")[1];
+    admin
+        .auth()
+        .verifyIdToken(accessToken)
+        .then((decoded) => {
+            req.decoded = decoded;
+            next();
+        })
+        .catch((err) => {
+            res.status(403).send({ message: "Forbidden Access", error: err });
+        });
+};
 
 app.get("/", (req, res) => {
     res.send("Welcome to KindleVent....");
@@ -28,17 +53,37 @@ const eventColl = client.db("kindleVent-DB").collection("events");
 
 async function run() {
     try {
-        // ?GET API
-        app.get("/events/all", async (req, res) => {
+        // ?All Events GET API
+        app.get("/event/upcoming", async (req, res) => {
             const result = await eventColl.find().toArray();
             res.send(result);
         });
 
-        // Send a ping to confirm a successful connection
-        // await client.db("admin").command({ ping: 1 });
-        console.log(
-            "Pinged your deployment. You successfully connected to MongoDB!"
-        );
+        // ?Joined Events GET API
+        app.get("/events/joined", verifyToken, async (req, res) => {
+            const userEmail = req.query.email;
+            const tokenEmail = req?.decoded?.email;
+            if (userEmail !== tokenEmail) {
+                return res.status(403).send({ message: "Forbidden Access" });
+            }
+            const query = {
+                "participants.email": userEmail,
+            };
+            const projection = {
+                participants: 0, //? exclude the participants field
+            };
+            const result = await eventColl
+                .find(query, { projection })
+                .toArray();
+            res.send(result);
+        });
+
+        // ? Create Events POST API
+        app.post("/event/create", async (req, res) => {
+            const doc = req.body;
+            const result = await eventColl.insertOne(doc);
+            res.send({ message: "data added successfully", result: result });
+        });
     } finally {
     }
 }
